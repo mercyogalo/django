cc5611a7aebde87daff43db735120d5309623005
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2024-11-12 13:23:30 +0300

    created a model for the form

--- ringshop/rings/models.py
+++ ringshop/rings/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class ring(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.EmailField()
+    age=models.IntegerField()
+    phone=models.CharField(max_length=100)
+    image=models.ImageField(upload_to='images/')
+    
+    
+    
+        
+    
